425498b50864b4f3da6dd73a6d73a7b000721673
jiang-rong <2605934892@qq.com>
2019-04-26 11:59:26 +0800

    some date updated

---
+++ api/rent.php
@@ -0,0 +1,37 @@
+<?php
+echo $_POST['name'];
+echo $_POST['age'];
+// header("Content-Type:text/html;charset=utf-8");
+// $page=$_GET['page'];
+// $arr=json_decode('[{
+// 			"imgUrl": "static/img/15047728744306.png",
+// 			"id": "20202",
+// 			"title":"TT8可排位★37V12王者★6烈龙4炼狱★王者之力之刺之击之光之戮之武之零★火麒麟★黑龙★雷神★死神★黑武士★天龙秘银★毁灭★激光★6副6刀★幻兽系列★悦动悦心★具体看图",
+// 			"gameName":"创越火线",
+// 			"lease":2,
+// 			"price":"6.5",
+// 			"foreMoney":10,
+// 			"rentTimes":2
+// 		},{
+// 			"imgUrl": "static/img/15047728744306.png",
+// 			"id": "20202",
+// 			"title":"TT8可排位★37V12王者★6烈龙4炼狱★王者之力之刺之击之光之戮之武之零★火麒麟★黑龙★雷神★死神★黑武士★天龙秘银★毁灭★激光★6副6刀★幻兽系列★悦动悦心★具体看图",
+// 			"gameName":"创越火线",
+// 			"lease":2,
+// 			"price":"6.5",
+// 			"foreMoney":10,
+// 			"rentTimes":2
+// 		}]',true);
+// $type=$_GET['type'];
+// if($type=="218"||$type=="all"){
+// 	$newarr=array_merge($arr);
+// }else{
+// 	$newarr=[];
+// }
+
+
+
+// $startnum=($page-1)*10;
+// 	echo json_encode(array("flag"=>true,"data"=>array_splice($newarr,$startnum,$startnum+10)));
+
+ ?>
